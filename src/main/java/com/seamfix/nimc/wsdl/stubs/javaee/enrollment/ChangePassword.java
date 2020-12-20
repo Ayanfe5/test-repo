cf2dcccbb6e5a28dab4eb1a1278a43cbe3fe21b1
Akinkunmi Morakinyo <amorakinyo@seamfix.com>
2020-12-20 16:24:48 +0100

    #MK-9192 - added the NIMC javaee stubs

---
+++ src/main/java/com/seamfix/nimc/wsdl/stubs/javaee/enrollment/ChangePassword.java
@@ -0,0 +1,87 @@
+
+package com.seamfix.nimc.wsdl.stubs.javaee.enrollment;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for changePassword complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="changePassword">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="token" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="newpassword" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "changePassword", propOrder = {
+    "token",
+    "newpassword"
+})
+public class ChangePassword {
+
+    protected String token;
+    protected String newpassword;
+
+    /**
+     * Gets the value of the token property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * Sets the value of the token property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setToken(String value) {
+        this.token = value;
+    }
+
+    /**
+     * Gets the value of the newpassword property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNewpassword() {
+        return newpassword;
+    }
+
+    /**
+     * Sets the value of the newpassword property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNewpassword(String value) {
+        this.newpassword = value;
+    }
+
+}
