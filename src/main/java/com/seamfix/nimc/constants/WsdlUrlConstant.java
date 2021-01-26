b885632de6f8d1429a2f4775d7519549c8017a3b
Morakinyo Akinkunmi <amorakinyo@seamfix.com>
2021-01-26 09:09:29 +0100

    #NIMC-1117 - Updated DataReception artifacts path

--- src/main/java/com/seamfix/nimc/constants/WsdlUrlConstant.java
+++ src/main/java/com/seamfix/nimc/constants/WsdlUrlConstant.java
@@ -4,5 +4,5 @@ import java.net.URL;
 
 public class WsdlUrlConstant {
     public static final URL SIM_REG_WSDL_FILE_URL = WsdlUrlConstant.class.getClassLoader().getResource("soapresources/nimc/identitysearch/IdentitySearch.wsdl");
-    public static final URL DATA_RECEPTION_WSDL_FILE_URL = WsdlUrlConstant.class.getClassLoader().getResource("soapresources/nimc/identitysearch/DataReception.wsdl");
+    public static final URL DATA_RECEPTION_WSDL_FILE_URL = WsdlUrlConstant.class.getClassLoader().getResource("soapresources/nimc/datareception/DataReception.wsdl");
 }
